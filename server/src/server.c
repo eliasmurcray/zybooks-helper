21e87abc57027a783a2177c0de745f0365598f69
EC2 Default User <ec2-user@ip-172-31-49-249.ec2.internal>
2024-12-29 09:06:33 +0000

    Add SSL support

--- server/src/server.c
+++ server/src/server.c
@@ -1,38 +1,43 @@
+#include <openssl/ssl.h>
+#include <openssl/err.h>
 #include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-void handle_connection(int client_fd, struct sockaddr_in *client_addr) {
-  char buffer[1024];
+#define SSLCERT_PATH "/etc/letsencrypt/live/zybooks.eliasmurcray.me/fullchain.pem"
+#define SSLKEY_PATH "/etc/letsencrypt/live/zybooks.eliasmurcray.me/privkey.pem"
+
+void handle_connection(SSL *ssl, struct sockaddr_in *client_addr) {
+  char buffer[1024], *request = NULL;
   size_t total_bytes = 0;
-  char *request = NULL;
   for (;;) {
-    ssize_t bytes_read = read(client_fd, buffer, sizeof(buffer) - 1);
-    if (bytes_read < 0) return;
+    ssize_t bytes_read = SSL_read(ssl, buffer, sizeof(buffer) - 1);
+    if (bytes_read < 0) {
+      perror("read");
+      return;
+    }
     if (bytes_read == 0) break;
     buffer[bytes_read] = '\0';
     char *new_request = realloc(request, total_bytes + bytes_read + 1);
     if (!new_request) {
       perror("realloc");
       free(request);
-      goto internal_server_error;
       return;
     }
     request = new_request;
     memcpy(request + total_bytes, buffer, bytes_read);
     total_bytes += bytes_read;
     request[total_bytes] = '\0';
-    if (strstr(request, "\r\n\r\n")) {
-      break;
-    }
+    if (strstr(request, "\r\n\r\n")) break;
   }
+  printf("%s\n", request);
   char client_ip[INET_ADDRSTRLEN];
   if (inet_ntop(AF_INET, &client_addr->sin_addr, client_ip, INET_ADDRSTRLEN) == NULL) {
     perror("inet_ntop");
     free(request);
-    goto internal_server_error;
+    return;
   }
   char *method = NULL, *path = NULL;
   if (request && strstr(request, "\r\n\r\n")) {
@@ -40,35 +45,38 @@ void handle_connection(int client_fd, struct sockaddr_in *client_addr) {
     path = strtok(NULL, " ");
     if (!method || !path) {
       free(request);
-      goto internal_server_error;
+      return;
     }
   }
   printf("%s %s %s\n", method, path, client_ip);
-  if (strcmp(method, "POST") != 0) {
+  /*if (strcmp(method, "POST") != 0) {
     free(request);
-    goto internal_server_error;
-  }
+    return;
+  }*/
   free(request);
   const char *response =
-      "HTTP/1.1 200 OK\r\n"
-      "Content-Type: application/json\r\n"
-      "Content-Length: 13\r\n"
-      "\r\n"
-      "{\"foo\":\"bar\"}";
-  write(client_fd, response, strlen(response));
-  return;
-internal_server_error: {
-  const char *response =
-      "HTTP/1.1 404 Not Found\r\n"
-      "Content-Type: text/plain\r\n"
-      "Content-Length: 0\r\n"
-      "\r\n";
-  write(client_fd, response, strlen(response));
-  return;
-}
+    "HTTP/1.1 200 OK\r\n"
+    "Content-Type: text/plain\r\n"
+    "Content-Length: 11\r\n"
+    "\r\n"
+    "Hello world";
+  SSL_write(ssl, response, strlen(response));
 }
 
 int main() {
+  SSL_library_init();
+  OpenSSL_add_all_algorithms();
+  SSL_load_error_strings();
+  SSL_CTX *ctx = SSL_CTX_new(TLS_server_method());
+  if (!ctx) {
+    ERR_print_errors_fp(stderr);
+    return 1;
+  }
+  if (SSL_CTX_use_certificate_file(ctx, SSLCERT_PATH, SSL_FILETYPE_PEM) <= 0 ||
+      SSL_CTX_use_PrivateKey_file(ctx, SSLKEY_PATH, SSL_FILETYPE_PEM) <= 0) {
+    ERR_print_errors_fp(stderr);
+    return 1;
+  }
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (server_fd < 0) {
     perror("Socket creation failed");
@@ -80,9 +88,9 @@ int main() {
     return 1;
   }
   struct sockaddr_in addr = {
-      .sin_family = AF_INET,
-      .sin_port = htons(3000),
-      .sin_addr.s_addr = INADDR_ANY};
+    .sin_family = AF_INET,
+    .sin_port = htons(443),
+    .sin_addr.s_addr = INADDR_ANY};
   if (bind(server_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
     perror("bind");
     close(server_fd);
@@ -93,7 +101,7 @@ int main() {
     close(server_fd);
     return 1;
   }
-  printf("Listening on port 3000...\n");
+  printf("Listening on port 443...\n");
   while (1) {
     struct sockaddr_in client_addr;
     socklen_t client_len = sizeof(client_addr);
@@ -102,7 +110,15 @@ int main() {
       perror("accept");
       continue;
     }
-    handle_connection(client_fd, &client_addr);
+    SSL *ssl = SSL_new(ctx);
+    SSL_set_fd(ssl, client_fd);
+    if (SSL_accept(ssl) <= 0) {
+      ERR_print_errors_fp(stderr);
+    } else {
+      handle_connection(ssl, &client_addr);
+    }
+    SSL_shutdown(ssl);
+    SSL_free(ssl);
     close(client_fd);
   }
   close(server_fd);
